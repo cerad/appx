7b9ae5d7bef6c88273fbb62a1f0219582763b136
Art (giles) Hundiak <ahundiak@gmail.com>
2015-03-28 14:09:06 -0500

    htaccess and router.php

--- web/app.php
+++ web/app.php
@@ -1,5 +1,11 @@
 <?php
 
+if ($_SERVER["REQUEST_URI"] == '/')
+{
+  if (file_exists('./index.php' )) { require 'index.php';  return; }
+  if (file_exists('./index.html')) { require 'index.html'; return; }
+}
+
 require '../vendor/autoload.php';
 
 use Pimple\Container;
